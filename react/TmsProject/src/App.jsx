3afc31d7a07d61fabbc414300f403ba98da7e525
FirasMoosaMohammed <firasmohamed0@gmail.com>
2025-07-25 00:50:31 +0530

    Protected admin route with ProtectedRoute component

--- react/TmsProject/src/App.jsx
+++ react/TmsProject/src/App.jsx
@@ -8,6 +8,7 @@ import "./bootstrap.min.css";
 import "./App.css";
 import { Toaster } from "react-hot-toast";
 import { Route, Routes } from "react-router-dom";
+import ProtectedRoute from "./pages/ProtectedRoute";
 
 function App() {
   return (
@@ -17,8 +18,22 @@ function App() {
         <Route path="/packages/:id" element={<PackageDetail />} />
         <Route path="/schedules/:id" element={<ScheduleDetail />} />
         <Route path="/login" element={<Login />} />
-        <Route path="/admin" element={<AdminHome />} />
-        <Route path="/admin/enquiries" element={<EnquiryList />} />
+        <Route
+          path="/admin"
+          element={
+            <ProtectedRoute>
+              <AdminHome />
+            </ProtectedRoute>
+          }
+        />
+        <Route
+          path="/admin/enquiries"
+          element={
+            <ProtectedRoute>
+              <EnquiryList />
+            </ProtectedRoute>
+          }
+        />
       </Routes>
       <Toaster position="top-right" />
     </div>
